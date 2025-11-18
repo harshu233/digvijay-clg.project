425131cc6e2a97663c0c19442ef012e88f7d0a95
Harshavardhan Bhosale <harshavardhanbhosale513@gmail.com>
2025-11-19 00:09:17 +0530

    msg

--- EventMgmt/src/main/java/com/eventmgmt/servlet/AdminEventServlet.java
+++ EventMgmt/src/main/java/com/eventmgmt/servlet/AdminEventServlet.java
@@ -11,14 +11,17 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class AdminEventServlet extends HttpServlet {
-    private SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+
+    private SimpleDateFormat formFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+    private SimpleDateFormat dbFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession s = req.getSession(false);
         if (s == null || s.getAttribute("user") == null) {
-            resp.sendRedirect(req.getContextPath()+"/login.jsp");
+            resp.sendRedirect(req.getContextPath() + "/login.jsp");
             return;
         }
+
         User u = (User) s.getAttribute("user");
         if (!u.isAdmin()) {
             resp.sendError(HttpServletResponse.SC_FORBIDDEN);
@@ -30,32 +33,50 @@ public class AdminEventServlet extends HttpServlet {
 
         try {
             if ("add".equals(action)) {
+
                 Event e = new Event();
                 e.setTitle(req.getParameter("title"));
                 e.setDescription(req.getParameter("description"));
                 e.setLocation(req.getParameter("location"));
-                Date sd = format.parse(req.getParameter("start_date"));
-                Date ed = format.parse(req.getParameter("end_date"));
-                e.setStartDate(sd); e.setEndDate(ed);
+
+                // Parse input date (Date object) from HTML form
+                Date sd = formFormat.parse(req.getParameter("start_date"));
+                Date ed = formFormat.parse(req.getParameter("end_date"));
+
+                // Convert to string for Event.java
+                e.setStartDate(dbFormat.format(sd));
+                e.setEndDate(dbFormat.format(ed));
+
                 e.setCapacity(Integer.parseInt(req.getParameter("capacity")));
+
                 dao.addEvent(e);
                 resp.sendRedirect("admin/events_admin.jsp?msg=added");
+
             } else if ("update".equals(action)) {
+
                 Event e = new Event();
                 e.setId(Integer.parseInt(req.getParameter("id")));
                 e.setTitle(req.getParameter("title"));
                 e.setDescription(req.getParameter("description"));
                 e.setLocation(req.getParameter("location"));
-                Date sd = format.parse(req.getParameter("start_date"));
-                Date ed = format.parse(req.getParameter("end_date"));
-                e.setStartDate(sd); e.setEndDate(ed);
+
+                Date sd = formFormat.parse(req.getParameter("start_date"));
+                Date ed = formFormat.parse(req.getParameter("end_date"));
+
+                e.setStartDate(dbFormat.format(sd));
+                e.setEndDate(dbFormat.format(ed));
+
                 e.setCapacity(Integer.parseInt(req.getParameter("capacity")));
+
                 dao.updateEvent(e);
                 resp.sendRedirect("admin/events_admin.jsp?msg=updated");
+
             } else if ("delete".equals(action)) {
+
                 int id = Integer.parseInt(req.getParameter("id"));
                 dao.deleteEvent(id);
                 resp.sendRedirect("admin/events_admin.jsp?msg=deleted");
+
             } else {
                 resp.sendRedirect("admin/events_admin.jsp");
             }
